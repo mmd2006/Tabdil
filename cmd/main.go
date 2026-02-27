710ca2e682230fa795c039786135d456576ec54b
mmd2006 <reza1385312@gmail.com>
2026-02-27 20:29:45 +0330

    modify main

--- cmd/main.go
+++ cmd/main.go
@@ -11,15 +11,13 @@ import (
 	"Tabdil/internal/service"
 )
 
-// نگهداری وضعیت هر کاربر
 var userState = make(map[int64]string)
 
-// تابع کمکی برای ارسال منوی اصلی
 func sendMainMenu(bot *tgbotapi.BotAPI, chatID int64, text string) {
-	keyboard := tgbotapi.NewInlineKeyboardMarkup(
-		tgbotapi.NewInlineKeyboardRow(
-			tgbotapi.NewInlineKeyboardButtonData("محاسبه طول", "length"),
-			tgbotapi.NewInlineKeyboardButtonData("محاسبه ارز", "currency"),
+	keyboard := tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("محاسبه طول"),
+			tgbotapi.NewKeyboardButton("محاسبه ارز"),
 		),
 	)
 
@@ -28,7 +26,6 @@ func sendMainMenu(bot *tgbotapi.BotAPI, chatID int64, text string) {
 	bot.Send(msg)
 }
 
-// تابع کمکی برای ارسال پیام ساده
 func send(bot *tgbotapi.BotAPI, chatID int64, text string) {
 	msg := tgbotapi.NewMessage(chatID, text)
 	bot.Send(msg)
@@ -36,7 +33,6 @@ func send(bot *tgbotapi.BotAPI, chatID int64, text string) {
 
 func main() {
 
-	// خواندن فایل .env
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("No .env file found")
@@ -60,171 +56,182 @@ func main() {
 
 	for update := range updates {
 
-		// ===============================
-		// هندل پیام‌های متنی
-		// ===============================
-		if update.Message != nil {
-			chatID := update.Message.Chat.ID
-			text := update.Message.Text
+		if update.Message == nil {
+			continue
+		}
 
-			// دستور /start
-			if text == "/start" {
-				sendMainMenu(bot, chatID, "سلام \nبه بات تبدیل خوش آمدید\nیک گزینه انتخاب کن:")
+		chatID := update.Message.Chat.ID
+		text := update.Message.Text
+
+		// start
+		if text == "/start" {
+			userState[chatID] = ""
+			sendMainMenu(bot, chatID, "سلام 👋\nبه بات تبدیل خوش آمدید\nیک گزینه انتخاب کن:")
+			continue
+		}
+
+		// دکمه محاسبه طول
+		if text == "محاسبه طول" {
+			lengthKeyboard := tgbotapi.NewReplyKeyboard(
+				tgbotapi.NewKeyboardButtonRow(
+					tgbotapi.NewKeyboardButton("KM ➜ Mile"),
+					tgbotapi.NewKeyboardButton("KG ➜ Pound"),
+				),
+				tgbotapi.NewKeyboardButtonRow(
+					tgbotapi.NewKeyboardButton("CM ➜ Foot"),
+					tgbotapi.NewKeyboardButton("megabit ➜ megabyte"),
+				),
+				tgbotapi.NewKeyboardButtonRow(
+					tgbotapi.NewKeyboardButton("بازگشت به منوی اصلی"),
+				),
+			)
+
+			msg := tgbotapi.NewMessage(chatID, "یک نوع تبدیل انتخاب کن:")
+			msg.ReplyMarkup = lengthKeyboard
+			bot.Send(msg)
+			continue
+		}
+
+		// بازگشت
+		if text == "بازگشت به منوی اصلی" {
+			userState[chatID] = ""
+			sendMainMenu(bot, chatID, "منوی اصلی:")
+			continue
+		}
+
+		// دکمه محاسبه ارز
+		if text == "محاسبه ارز" {
+
+			rate, err := service.FetchDollarRate()
+			if err != nil {
+				send(bot, chatID, "خطا در دریافت نرخ دلار 😕")
 				continue
 			}
 
-			// اگر کاربر در حالت وارد کردن عدد است
-			switch userState[chatID] {
+			dollarInToman := rate / 10
+			userState[chatID] = "currency"
 
-			case "km_to_mile":
-				value, err := strconv.ParseFloat(text, 64)
-				if err != nil {
-					send(bot, chatID, "عدد معتبر وارد کن")
-					continue
-				}
+			send(bot, chatID,
+				"💵 قیمت روز دلار: "+
+					strconv.FormatFloat(dollarInToman, 'f', 0, 64)+
+					" تومان\n\nمقدار تومان را وارد کنید:")
 
-				result, err := service.KilometerToMile(value)
-				if err != nil {
-					send(bot, chatID, err.Error())
-					continue
-				}
-
-				resultText := strconv.FormatFloat(result, 'f', 4, 64) + " Mile"
-				userState[chatID] = ""
-				sendMainMenu(bot, chatID, "نتیجه: "+resultText+"\n\nیک گزینه دیگر انتخاب کن:")
-
-			case "kg_to_pound":
-				value, err := strconv.ParseFloat(text, 64)
-				if err != nil {
-					send(bot, chatID, "عدد معتبر وارد کن")
-					continue
-				}
-
-				result, err := service.KilogramToPound(value)
-				if err != nil {
-					send(bot, chatID, err.Error())
-					continue
-				}
-
-				resultText := strconv.FormatFloat(result, 'f', 4, 64) + " Pound"
-				userState[chatID] = ""
-				sendMainMenu(bot, chatID, "نتیجه: "+resultText+"\n\nیک گزینه دیگر انتخاب کن:")
-
-			case "cm_to_foot":
-				value, err := strconv.ParseFloat(text, 64)
-				if err != nil {
-					send(bot, chatID, err.Error())
-					continue
-				}
-
-				result, err := service.CentimeterToFoot(value)
-				if err != nil {
-					send(bot, chatID, err.Error())
-					continue
-				}
-
-				userState[chatID] = ""
-				sendMainMenu(bot, chatID, "نتیجه :"+strconv.FormatFloat(result, 'f', 4, 64)+"Foot\n\n یک گزینه دیگر انتخاب کن :")
-
-			case "mbit_to_mbyte":
-				value, err := strconv.ParseFloat(text, 64)
-				if err != nil {
-					send(bot, chatID, "عدد معتبر وارد کن")
-					continue
-				}
-
-				result, err := service.MegabitToMegabyte(value)
-				if err != nil {
-					send(bot, chatID, err.Error())
-					continue
-
-				}
-
-				userState[chatID] = ""
-				sendMainMenu(bot, chatID,
-					"نتیجه: "+strconv.FormatFloat(result, 'f', 4, 64)+" MB\n\nیک گزینه دیگر انتخاب کن:")
-
-			case "currency":
-				value, err := strconv.ParseFloat(text, 64)
-				if err != nil {
-					send(bot, chatID, "عدد معتبر وارد کن")
-					continue
-				}
-
-				value = value * 10
-
-				result, err := service.ConvertTomanToDollarWithAPI(value)
-				if err != nil {
-					send(bot, chatID, "خطا در دریافت نرخ دلار: "+err.Error())
-					continue
-				}
-
-				userState[chatID] = ""
-				sendMainMenu(bot, chatID,
-					"نتیجه: "+strconv.FormatFloat(result, 'f', 4, 64)+" USD\n\nیک گزینه دیگر انتخاب کن:")
-			}
+			continue
 		}
 
-		// ===============================
-		// هندل دکمه‌ها
-		// ===============================
-		if update.CallbackQuery != nil {
-			chatID := update.CallbackQuery.Message.Chat.ID
-			data := update.CallbackQuery.Data
+		// انتخاب نوع تبدیل طول
+		switch text {
+		case "KM ➜ Mile":
+			userState[chatID] = "km_to_mile"
+			send(bot, chatID, "مقدار کیلومتر را وارد کن:")
+			continue
 
-			switch data {
-			case "length":
-				keyboard := tgbotapi.NewInlineKeyboardMarkup(
-					tgbotapi.NewInlineKeyboardRow(
-						tgbotapi.NewInlineKeyboardButtonData("KM ➜ Mile", "km_to_mile"),
-						tgbotapi.NewInlineKeyboardButtonData("KG ➜ Pound", "kg_to_pound"),
-					),
-					tgbotapi.NewInlineKeyboardRow(
-						tgbotapi.NewInlineKeyboardButtonData("CM ➜ Foot", "cm_to_foot"),
-						tgbotapi.NewInlineKeyboardButtonData("megabit ➜ megabyte", "mbit_to_mbyte"),
-					),
-				)
+		case "KG ➜ Pound":
+			userState[chatID] = "kg_to_pound"
+			send(bot, chatID, "مقدار کیلوگرم را وارد کن:")
+			continue
 
-				msg := tgbotapi.NewMessage(chatID, "یک نوع تبدیل انتخاب کن:")
-				msg.ReplyMarkup = keyboard
-				bot.Send(msg)
+		case "CM ➜ Foot":
+			userState[chatID] = "cm_to_foot"
+			send(bot, chatID, "مقدار سانتی‌متر را وارد کن:")
+			continue
 
-			case "km_to_mile":
-				userState[chatID] = "km_to_mile"
-				send(bot, chatID, "مقدار کیلومتر را وارد کن:")
+		case "megabit ➜ megabyte":
+			userState[chatID] = "mbit_to_mbyte"
+			send(bot, chatID, "مقدار مگابیت را وارد کن:")
+			continue
+		}
 
-			case "kg_to_pound":
-				userState[chatID] = "kg_to_pound"
-				send(bot, chatID, "مقدار کیلوگرم را وارد کن:")
-			case "cm_to_foot":
-				userState[chatID] = "cm_to_foot"
-				send(bot, chatID, "مقدار سانتی‌متر را وارد کن:")
+		// اگر کاربر عدد وارد کرده
+		switch userState[chatID] {
 
-			case "mbit_to_mbyte":
-				userState[chatID] = "mbit_to_mbyte"
-				send(bot, chatID, "مقدار مگابیت را وارد کن:")
-
-			case "currency":
-
-				rate, err := service.FetchDollarRate()
-				if err != nil {
-					send(bot, chatID, "خطا در دریافت نرخ دلار 😕")
-					break
-				}
-
-				dollarInToman := rate / 10
-
-				userState[chatID] = "currency"
-
-				send(bot, chatID,
-					"💵 قیمت روز دلار: "+
-						strconv.FormatFloat(dollarInToman, 'f', 0, 64)+
-						" تومان\n\nمقدار تومان را وارد کنید:")
+		case "km_to_mile":
+			value, err := strconv.ParseFloat(text, 64)
+			if err != nil {
+				send(bot, chatID, "عدد معتبر وارد کن")
+				continue
 			}
 
-			// پاسخ به Callback برای جلوگیری از حالت loading
-			callback := tgbotapi.NewCallback(update.CallbackQuery.ID, "")
-			bot.Request(callback)
+			result, err := service.KilometerToMile(value)
+			if err != nil {
+				send(bot, chatID, err.Error())
+				continue
+			}
+
+			userState[chatID] = ""
+			sendMainMenu(bot, chatID,
+				"نتیجه: "+strconv.FormatFloat(result, 'f', 4, 64)+" Mile\n\nیک گزینه دیگر انتخاب کن:")
+
+		case "kg_to_pound":
+			value, err := strconv.ParseFloat(text, 64)
+			if err != nil {
+				send(bot, chatID, "عدد معتبر وارد کن")
+				continue
+			}
+
+			result, err := service.KilogramToPound(value)
+			if err != nil {
+				send(bot, chatID, err.Error())
+				continue
+			}
+
+			userState[chatID] = ""
+			sendMainMenu(bot, chatID,
+				"نتیجه: "+strconv.FormatFloat(result, 'f', 4, 64)+" Pound\n\nیک گزینه دیگر انتخاب کن:")
+
+		case "cm_to_foot":
+			value, err := strconv.ParseFloat(text, 64)
+			if err != nil {
+				send(bot, chatID, "عدد معتبر وارد کن")
+				continue
+			}
+
+			result, err := service.CentimeterToFoot(value)
+			if err != nil {
+				send(bot, chatID, err.Error())
+				continue
+			}
+
+			userState[chatID] = ""
+			sendMainMenu(bot, chatID,
+				"نتیجه: "+strconv.FormatFloat(result, 'f', 4, 64)+" Foot\n\nیک گزینه دیگر انتخاب کن:")
+
+		case "mbit_to_mbyte":
+			value, err := strconv.ParseFloat(text, 64)
+			if err != nil {
+				send(bot, chatID, "عدد معتبر وارد کن")
+				continue
+			}
+
+			result, err := service.MegabitToMegabyte(value)
+			if err != nil {
+				send(bot, chatID, err.Error())
+				continue
+			}
+
+			userState[chatID] = ""
+			sendMainMenu(bot, chatID,
+				"نتیجه: "+strconv.FormatFloat(result, 'f', 4, 64)+" MB\n\nیک گزینه دیگر انتخاب کن:")
+
+		case "currency":
+			value, err := strconv.ParseFloat(text, 64)
+			if err != nil {
+				send(bot, chatID, "عدد معتبر وارد کن")
+				continue
+			}
+
+			// تبدیل تومان به ریال
+			value = value * 10
+
+			result, err := service.ConvertTomanToDollarWithAPI(value)
+			if err != nil {
+				send(bot, chatID, "خطا در دریافت نرخ دلار: "+err.Error())
+				continue
+			}
+
+			userState[chatID] = ""
+			sendMainMenu(bot, chatID,
+				"نتیجه: "+strconv.FormatFloat(result, 'f', 4, 64)+" USD\n\nیک گزینه دیگر انتخاب کن:")
 		}
 	}
 }
